56d09bc2844a6fcab2648a2799fb71b5ff48b07c
mrizkysolehudin <mrizkysolehudin11@gmail.com>
2023-11-23 15:34:23 +0700

    update password validation

--- app/Http/Requests/SignupRequest.php
+++ app/Http/Requests/SignupRequest.php
@@ -28,7 +28,7 @@ class SignupRequest extends FormRequest
             'password' => [
                 'required',
                 'confirmed',
-                Password::min(8)->mixedCase()->numbers()->symbols()
+                Password::min(8)
             ]
         ];
     }
